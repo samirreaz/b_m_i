cd7c1eb9ae9023ca6e89d741e78e80932f538590
Sam Sabbir <sabbirs773@gmail.com>
2022-02-17 17:19:07 +0600

    base design finished

---
+++ lib/home_page.dart
@@ -0,0 +1,187 @@
+import 'dart:math';
+
+import 'package:b_m_i/result_page.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({ Key? key }) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+ bool isMale = true;
+  double heightVal = 170;
+
+  int weight = 55;
+  int age = 18;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Body Mass Index'), centerTitle: true),
+      drawer: Drawer(),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Row(
+                  children: [
+                    m1Expanded(context, 'male'),
+                    const SizedBox(width: 15),
+                    m1Expanded(context, 'female'),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.blueGrey,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('Height',
+                          style: Theme.of(context).textTheme.headline2),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.baseline,
+                        textBaseline: TextBaseline.alphabetic,
+                        children: [
+                          Text(heightVal.toStringAsFixed(1),
+                              style: Theme.of(context).textTheme.headline1),
+                          Text('CM',
+                              style: Theme.of(context).textTheme.bodyText1),
+                        ],
+                      ),
+                      Slider(
+                        min: 90,
+                        max: 220,
+                        value: heightVal,
+                        onChanged: (newValue) => setState(() => heightVal = newValue),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Row(
+                  children: [
+                    m2Expanded(context, 'weight'),
+                    const SizedBox(width: 15),
+                    m2Expanded(context, 'age'),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              color: Colors.teal,
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height / 16,
+              child: TextButton(
+                onPressed: () {
+                  var result = weight / pow(heightVal / 100, 2);
+                  //print(result);
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          ResultPage(result: result, isMale: isMale, age: age),
+                    ),
+                  );
+                },
+                child: Text(
+                  'Calculate',
+                  style: Theme.of(context).textTheme.headline2,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Expanded m1Expanded(BuildContext context, String type) {
+    return Expanded(
+      child: GestureDetector(
+        onTap: () => setState(() => isMale = (type == 'male') ? true : false),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: (isMale && type == 'male') || (!isMale && type == 'female')
+                ? Colors.teal
+                : Colors.blueGrey,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(type == 'male' ? Icons.male : Icons.female, size: 90),
+              const Spacer(),
+              Text(
+                type == 'male' ? 'Male' : 'Female',
+                style: Theme.of(context).textTheme.headline2,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Expanded m2Expanded(BuildContext context, String type) {
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.blueGrey,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              type == 'age' ? 'Age' : 'Weight',
+              style: Theme.of(context).textTheme.headline2,
+            ),
+
+            Text(
+              type == 'age' ? '$age' : '$weight',
+              style: Theme.of(context).textTheme.headline1,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FloatingActionButton(
+                  heroTag: type == 'age' ? 'age--' : 'weight--',
+                  onPressed: () =>
+                      setState(() => type == 'age' ? age-- : weight--),
+                  child: const Icon(Icons.remove),
+                  mini: true,
+                ),
+                const SizedBox(width: 8),
+                FloatingActionButton(
+                  heroTag: type == 'age' ? 'age++' : 'weight++',
+                  onPressed: () =>
+                      setState(() => type == 'age' ? age++ : weight++),
+                  child: const Icon(Icons.add),
+                  mini: true,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
